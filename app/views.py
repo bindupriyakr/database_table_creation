911ad709712b81f946dd697c059d85f968403ada
kr bindupriya <krbindupriya@gmail.com>
2023-06-22 14:01:26 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from app.models import *
+# Create your views here.
+def display_topics(request):
+    topics=Topic.objects.all()
+    d={'topics':topics}
+    return render(request,'display_topics.html',d)
+
+def display_webpages(request):
+    webpages=Webpage.objects.all()
+    d={'webpages':webpages}
+    return render(request,'display_webpages.html',d)
